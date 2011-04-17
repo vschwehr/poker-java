ec2ffca11bd9bfcf54d8075bbf83dbf069e49149
vschwehr-fed <vschwehr9@gmail.com>
2011-04-17 01:37:33 -0600

    updated README

--- src/client/view/Main.java
+++ src/client/view/Main.java
@@ -14,18 +14,17 @@ import client.model.Deck;
 import client.model.Player;
 import client.socket.model.Client;
 
-
 public class Main {
-	
+
 	private static Deck deck;
 	private static final int SMALL_BLIND = 5;
 	private static final int BIG_BLIND = SMALL_BLIND * 2;
 	private static PlayerController pc;
-	
-	private Client client;
-	
+
+	// private Client client;
+
 	public Main(Client client) {
-		this.client = client;
+		// this.client = client;
 	}
 
 	public void start() {
@@ -36,33 +35,34 @@ public class Main {
 		players.add(new Player("Richmond", 500));
 		players.add(new Player("Richard", 500));
 		pc = new PlayerController(players);
-		
+
 		System.out.println("Welcome to schwehringHil's Texas Hold'em!");
-		System.out.println("Small blinds will be: $"+SMALL_BLIND);
-		System.out.println("Big blinds will be: $"+BIG_BLIND);
-		
+		System.out.println("Small blinds will be: $" + SMALL_BLIND);
+		System.out.println("Big blinds will be: $" + BIG_BLIND);
+
 		ScoreController scc = new ScoreController(players, SMALL_BLIND);
-		RoundController tc = new RoundController(players, scc);
-		
+		RoundController rc = new RoundController(players, scc);
+
 		ShuffleController sc = new ShuffleController();
 		sc.doShuffle();
 		deck = sc.getDeck();
 		CardController cc = new CardController(players, deck);
 		cc.dealDeck();
-		
-		for(Player player : pc.getPlayers()) {
+
+		for (Player player : pc.getPlayers()) {
 			System.out.println(player.printPlayer());
 		}
-		
-		while(tc.hasNext()) {
-			Player currentPlayer = tc.nextPlayer();
-			//do blinds
-			//wait for player input to bet
+
+		while (rc.hasNext()) {
+			// Player currentPlayer = rc.nextPlayer();
+			// do blinds
+			// wait for player input to bet
 		}
-		
-		CommunityCards communityCards = new CommunityCards(deck, players.size()*2);
+
+		CommunityCards communityCards = new CommunityCards(deck,
+				players.size() * 2);
 		System.out.println("Community Cards:");
-		for(Card card : communityCards) {
+		for (Card card : communityCards) {
 			System.out.println(card.toString());
 		}
 		HandController hc = new HandController(players, communityCards);
@@ -70,9 +70,6 @@ public class Main {
 		System.out.println(hc.getcCardPoints());
 	}
 
-	/**
-	 * @param args
-	 */
 	@Deprecated
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -83,38 +80,41 @@ public class Main {
 		players.add(new Player("Richmond", 500));
 		players.add(new Player("Richard", 500));
 		pc = new PlayerController(players);
-		
+
 		System.out.println("Welcome to schwehringHil's Texas Hold'em!");
-		System.out.println("Small blinds will be: $"+SMALL_BLIND);
-		System.out.println("Big blinds will be: $"+BIG_BLIND);
-		
-		ScoreController scc = new ScoreController(players, SMALL_BLIND);
-		RoundController rc = new RoundController(players, scc);
-		
+		System.out.println("Small blinds will be: $" + SMALL_BLIND);
+		System.out.println("Big blinds will be: $" + BIG_BLIND);
+
+		//ScoreController scc = new ScoreController(players, SMALL_BLIND);
+		//RoundController rc = new RoundController(players, scc);
+
 		ShuffleController sc = new ShuffleController();
 		sc.doShuffle();
 		deck = sc.getDeck();
 		CardController cc = new CardController(players, deck);
 		cc.dealDeck();
-		
-		for(Player player : pc.getPlayers()) {
+
+		for (Player player : pc.getPlayers()) {
 			System.out.println(player.printPlayer());
 		}
-		
-		while(rc.hasNext()) {
+
+		/*while (rc.hasNext()) {
 			Player currentPlayer = rc.nextPlayer();
-			//do blinds
-			//wait for player input to bet
-		}
-		
-		CommunityCards communityCards = new CommunityCards(deck, players.size()*2);
+			// do blinds
+			// wait for player input to bet
+		}*/
+
+		CommunityCards communityCards = new CommunityCards(deck,
+				players.size() * 2);
 		System.out.println("Community Cards:");
-		for(Card card : communityCards) {
+
+		for (Card card : communityCards) {
 			System.out.println(card.toString());
 		}
+
 		HandController hc = new HandController(players, communityCards);
 		hc.doAnalyzeCCards();
-		System.out.println(hc.getcCardPoints());
+		System.out.println("Community Card points: " + hc.getcCardPoints());
 	}
 
 }
